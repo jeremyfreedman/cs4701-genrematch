4ce57fbac88730df13c79d4f27dafca7c9134f6b
Jeremy Freedman <jmf365@cornell.edu>
2022-05-14 16:45:02 -0400

    truncated feature extraction; frontend work; pickled decision tree

--- __main__.py
+++ __main__.py
@@ -22,18 +22,27 @@ if __name__ == "__main__":
     print("Available models:")
     for i in range(len(models)):
         print(f"[{i+1}] {models[i][0]}")
-    print("Please pick a model (specify the number): ", end="")
+    print("> Please pick a model (specify the number): ", end="")
     mdl = int(input())-1
-    print(f"You chose {models[mdl][0]}")
+    print(f"> You chose {models[mdl][0]}")
     with open(models[mdl][1], 'rb') as f:
         clf = pickle.load(f)
     with open("data/words.txt") as f:
         words = f.readlines()
     words = ast.literal_eval(words[0])
     word_dict = {n : 0 for n in words}
-    print(f"Input song lyrics for genre prediction:")
-    lyrics = input()
-    for w in parse_input(lyrics):
+    print(f"> Input song lyrics for genre prediction:\n>> Press CTRL+D or 3 newlines to end input!")
+    lyrics_arr = []
+    try: # run until CTRL+D pressed
+        while True:
+            lyrics_arr.append(input())
+            if lyrics_arr[-3:] == ['', '', '']:
+                break
+    except EOFError:
+        pass
+    lyrics = "\n".join(lyrics_arr)
+    lyrics_parsed = parse_input(lyrics)
+    for w in lyrics_parsed:
         if w in word_dict.keys():
             word_dict[w] += 1
-    print(clf.predict(np.array(list(word_dict.values()) + [0]).reshape(1,-1))[0])
+    print(f"> This sounds like {clf.predict(np.array(list(word_dict.values()) + [len(set(lyrics_parsed))]).reshape(1,-1))[0]}!")
